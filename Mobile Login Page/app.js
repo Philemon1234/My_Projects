3e7c6884f54c8dc34b0658f2dc0651ca92e0693e
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2023-09-12 12:36:58 +0300

    Modified project files

--- Mobile Login Page/app.js
+++ Mobile Login Page/app.js
@@ -1,3 +1,15 @@
-function clickButton() {
-    alert("clicked")
+let homePage = document.getElementById("home-page")
+let signUpPage = document.getElementById("sign-up-page")
+let loginPage = document.getElementById("login-page")
+
+function showSignupPage() {
+    homePage.style.display = 'none'
+    loginPage.style.display = 'none'
+    signUpPage.style.display = 'block'
+}
+
+function showLoginPage() {
+    homePage.style.display = 'none'
+    loginPage.style.display = 'block'
+    signUpPage.style.display = 'none'
 }
