34ef018c213f02be2247990283d381fc34a50d23
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2023-09-29 17:10:43 +0300

    Added Testimonial slider functionality

--- Yufat Website/js/script.js
+++ Yufat Website/js/script.js
@@ -2,11 +2,26 @@ let openNav = document.getElementById("open");
 let closeNav = document.getElementById("close");
 let nav_links = document.querySelector(".nav_links");
 
-openNav.addEventListener("click", function() {
+openNav.addEventListener("click", function () {
     nav_links.classList.add("show_nav");
 })
 
-closeNav.addEventListener("click", function() {
+closeNav.addEventListener("click", function () {
     nav_links.classList.remove("show_nav");
 })
 
+
+// Testimonial Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
